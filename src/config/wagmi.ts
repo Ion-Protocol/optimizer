856cafebbf0521d0411bce11ff803a07b73443f4
Kyle <noll.kyle@gmail.com>
2025-02-12 15:04:22 -0900

    Add web3 wallet integration with RainbowKit, Wagmi, and React Query

---
+++ src/config/wagmi.ts
@@ -0,0 +1,30 @@
+import { getDefaultConfig, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { bitgetWallet, ledgerWallet } from "@rainbow-me/rainbowkit/wallets";
+import { QueryClient } from "@tanstack/react-query";
+import { mainnet } from "viem/chains";
+import { fallback, http } from "wagmi";
+
+const WALLET_CONNECT_PROJECT_ID = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
+const MAINNET_CHAINSTACK_URL = import.meta.env.VITE_PUBLIC_MAINNET_CHAINSTACK_URL;
+
+const { wallets } = getDefaultWallets();
+
+export const wagmiConfig = getDefaultConfig({
+  appName: "Optimizer",
+  projectId: WALLET_CONNECT_PROJECT_ID,
+  chains: [mainnet],
+  wallets: [
+    ...wallets,
+    {
+      groupName: "Other",
+      wallets: [bitgetWallet, ledgerWallet],
+    },
+  ],
+  ssr: false,
+  transports: {
+    [mainnet.id]: fallback([http(MAINNET_CHAINSTACK_URL)]),
+  },
+});
+
+export const queryClient = new QueryClient();
