ce5b8e48a38888d04f6bf20f1c740c43942f9e67
Rowdy <mroddy5280@gmail.com>
2025-02-20 15:29:03 -0700

    feat(Dashboard.tsx): updated inline styles

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
-import { VaultGroup } from "../types";
 import { useDashboard } from "../hooks/useDashboard";
-
+import { VaultGroup } from "../types";
 export function Dashboard() {
   const navigate = useNavigate();
   const { totalTvl, vaultGroupData, loading } = useDashboard();
@@ -11,21 +11,28 @@ export function Dashboard() {
   }
   return (
     <div>
-      <h1>Hemi Ecosystem Optimizer</h1>
+      <h1 className="text-2xl font-bold">Hemi Ecosystem Optimizer</h1>
       <p>Welcome to the dashboard page.</p>
       {loading ? <p>TVL: Loading...</p> : <p>TVL: {totalTvl}</p>}
-      <p style={{ fontWeight: "bold", marginTop: "50px" }}>Vaults:</p>
+      <p className="font-bold mt-10">Vaults:</p>
       {loading ? (
         <p>Vaults: Loading...</p>
       ) : (
         vaultGroupData.map((vaultGroup) => (
-          <div style={{ display: "flex", gap: "10px", justifyContent: "center" }} key={vaultGroup.vaultGroupKey}>
+          <div
+            className="flex gap-2 justify-center"
+            key={vaultGroup.vaultGroupKey}
+          >
             <p>{vaultGroup.vaultGroupKey}: </p>
             <p>{vaultGroup.tvl}</p>
             <p>{vaultGroup.apy}</p>
             <p>Protocols: </p>
             <p>{vaultGroup.protocols.join(", ")}</p>
-            <button onClick={() => handleClickExplore(vaultGroup.vaultGroupKey)}>Explore</button>
+            <Button
+              onClick={() => handleClickExplore(vaultGroup.vaultGroupKey)}
+            >
+              Explore
+            </Button>
           </div>
         ))
       )}
