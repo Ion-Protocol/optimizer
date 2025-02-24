8808281e5b2d8a749435e38a7afb8fcf8c67a248
Rowdy <mroddy5280@gmail.com>
2025-02-24 12:18:12 -0700

    fix(app.tsx): updated inline styles to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 import { WagmiProvider } from "wagmi";
 import "./App.css";
 import { AppRouter } from "./AppRouter";
 import { AppFooter } from "./components/AppFooter";
 import { AppHeader } from "./components/AppHeader";
 import { queryClient, wagmiConfig } from "./config/wagmi";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
@@ -15,19 +15,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           <BrowserRouter>
-            <div
-              style={{
-                position: "absolute",
-                width: "403px",
-                height: "403px",
-                left: "550px",
-                top: "-283px",
-                background: "#FF6C15",
-                opacity: 1,
-                filter: "blur(500px)",
-                zIndex: 0,
-              }}
-            />
+            <div className="absolute w-[403px] h-[403px] left-[550px] -top-[283px] bg-[#FF6C15] opacity-100 blur-[500px] z-0" />
             <div className="mx-auto w-[85%] min-w-[500px] max-w-[1500px] relative z-10">
               <AppHeader />
               <AppRouter />
