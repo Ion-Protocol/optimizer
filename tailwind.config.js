96fba860af728af97f92a417d5559bc79ce091bb
Rowdy <mroddy5280@gmail.com>
2025-02-24 11:20:35 -0700

    feat(Fonts): added base fonts
    might need to update. fonts in figma appear somewhat broken

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        body: ["PP Formula", "sans-serif"],
+        heading: ["Diatype", "serif"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
